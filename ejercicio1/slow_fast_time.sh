573b8863260b994665371b09c478cd5d0b557ac2
Javier Encinas Cortés <javiec93encinas@gmail.com>
2018-11-15 20:24:52 +0100

    Haciendo bien el primer ejercicio

--- ejercicio1/slow_fast_time.sh
+++ ejercicio1/slow_fast_time.sh
@@ -4,29 +4,46 @@
 Ninicio=15120
 Npaso=64
 Nfinal=16144 
-fDAT=slow_fast_time.dat
+#fDAT=slow_fast_time.dat
 fPNG=slow_fast_time.png
 
 # borrar el fichero DAT y el fichero PNG
 rm -f $fDAT fPNG
 
 # generar el fichero DAT vacío
-touch $fDAT
 
-echo "Running slow and fast..."
-# bucle para N desde P hasta Q
-for N in $(seq $Ninicio $Npaso $Nfinal); do
-	echo "N: $N / $Nfinal..."
+for i in $(seq 1 1 10); do
+	echo "Running slow and fast 10 times..."
+	touch "slowTime$i.dat"
+	touch "fastTime$idat"
+	# bucle para N desde Ninicio hasta Nfinal
+	for N in $(seq $Ninicio $Npaso $Nfinal); do
+		echo "N: $N / $Nfinal the slow program..."
 
-	# ejecutar los programas slow y fast consecutivamente con tamaño de matriz N
-	# para cada uno, filtrar la línea que contiene el tiempo y seleccionar la
-	# tercera columna (el valor del tiempo). Dejar los valores en variables
-	# para poder imprimirlos en la misma línea del fichero de datos
-	slowTime=$(./slow $N | grep 'time' | awk '{print $3}')
-	fastTime=$(./fast $N | grep 'time' | awk '{print $3}')
+		# ejecutar los programas slow y fast consecutivamente con tamaño de matriz N
+		# para cada uno, filtrar la línea que contiene el tiempo y seleccionar la
+		# tercera columna (el valor del tiempo). Dejar los valores en variables
+		# para poder imprimirlos en la misma línea del fichero de datos
+		slowTime=$(./slow $N | grep 'time' | awk '{print $3}')
 
-	echo "$N	$slowTime	$fastTime" >> $fDAT
+		echo "$N	$slowTime" >> slowTime$i.dat
+	done
+
+	# bucle para N desde Ninicio hasta Nfinal
+	for N in $(seq $Ninicio $Npaso $Nfinal); do
+		echo "N: $N / $Nfinal the fast programm..."
+
+		# ejecutar los programas slow y fast consecutivamente con tamaño de matriz N
+		# para cada uno, filtrar la línea que contiene el tiempo y seleccionar la
+		# tercera columna (el valor del tiempo). Dejar los valores en variables
+		# para poder imprimirlos en la misma línea del fichero de datos
+		fastTime=$(./fast $N | grep 'time' | awk '{print $3}')
+
+		echo "$N	$fastTime" >> fastTime$i.dat
+	done
 done
+
+#Ahora hay que calcular la media de cada fichero slow y fast con un numero y volcarlo en otro fichero que tenga todas las medias y plotearlo
 
 echo "Generating plot..."
 # llamar a gnuplot para generar el gráfico y pasarle directamente por la entrada
